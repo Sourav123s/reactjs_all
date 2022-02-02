360f5b2c221ef4e4d5ed57aaed729ada36b06249
sourav <paramaniksourav10111999@gmail.com>
2022-02-02 09:50:33 +0530

    first commit

---
+++ src/components/todo.js
@@ -0,0 +1,12 @@
+function todo() {
+  return (
+    <div className="card">
+      <h3>TITLE</h3>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default todo;
